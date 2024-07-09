6e0726a24e13009dd52b5c4924d5027525261a6c
tomisin110 <ayoemma188@gmail.com>
2024-07-09 21:46:00 +0100

    dummy commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { navIcons, navItems } from "../constants/index";
+
+
+const Navbar = () => {
+  return (
+    <nav className="top-0 z-50 py-3 bg-gray-500 h-24">
+        <div className="container px-4 mx-auto relative text-sm border">
+        <div className="flex justify-between items-center">
+                <div className="flex items-center flex-shrink-0">
+                    
+                    <span className="text-5xl tracking-tight text-white font-playfair font-bold ">HAWVEY</span>
+                </div>
+                <ul className="hidden lg:flex ml-14 space-x-12">
+                        {navItems.map((item, index)=>(
+                            <li key={index}>
+                                <a className="text-white" href={item.href}>{item.label}</a>
+                            </li>
+                        ) )}
+
+                    </ul>
+                    <div className="hidden lg:flex justify-center space-x-12 items-center">
+                        {navIcons.map((item, index) =>(
+                            <button className="border w-6 h-6" key={index}><a href="" className="border">{item.icon}</a></button>
+                            
+                        ))};
+                    </div> 
+                </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.jsx
@@ -0,0 +1,13 @@
+import { Search, ShoppingCart } from "lucide";
+
+export const navItems = [
+    { label: "New collection", href: "#" },
+    { label: "About Us", href: "#" },
+    { label: "Catalog", href: "#" },
+    { label: "Contact", href: "#" },
+  ];
+
+  export const navIcons =[
+    {icon:<Search />},
+    {icon:<ShoppingCart />},
+  ]
